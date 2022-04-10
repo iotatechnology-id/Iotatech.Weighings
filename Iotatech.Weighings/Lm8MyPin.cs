a221834d60f29958382d906445d4c5f1cfe8d1c9
Seiko Santana <santanaaseiko@gmail.com>
2022-04-10 18:16:51 +0700

    First commit

---
+++ Iotatech.Weighings/Lm8MyPin.cs
@@ -0,0 +1,86 @@
+﻿using System.Net;
+using FluentModbus;
+
+namespace Iotatech.Weighings;
+
+/// <summary>
+///     LM8MyPin abstraction above the Modbus communication
+/// </summary>
+public class Lm8MyPin
+{
+    /// <summary>
+    ///     Initialize an instance of LM8MyPin with Modbus over TCP adapter
+    /// </summary>
+    /// <param name="adapterIpAddress">IP address of the adapter</param>
+    public Lm8MyPin(IPAddress adapterIpAddress)
+    {
+        AdapterIpAddress = adapterIpAddress;
+        ModbusClient = new ModbusTcpClient();
+    }
+
+    /// <summary>
+    ///     Initialize LM8MyPin with Modbus over RTU
+    /// </summary>
+    /// <param name="serialPortName">The serial port name</param>
+    public Lm8MyPin(string serialPortName)
+    {
+        SerialPortName = serialPortName;
+        ModbusClient = new ModbusRtuClient();
+    }
+
+    /// <summary>
+    ///     initialize LM8MyPin from with an existing ModbusClient
+    /// </summary>
+    /// <param name="modbusClient">ModbusClient instance</param>
+    public Lm8MyPin(ModbusClient modbusClient)
+    {
+        ModbusClient = modbusClient;
+    }
+
+    private IPAddress? AdapterIpAddress { get; }
+    private ModbusClient ModbusClient { get; }
+    private string? SerialPortName { get; }
+
+    /// <summary>
+    ///     Connect to the adapter or serial port for communication.
+    /// </summary>
+    public void Connect()
+    {
+        if (ModbusClient is ModbusTcpClient tcpClient)
+            tcpClient.Connect(AdapterIpAddress);
+        else if (ModbusClient is ModbusRtuClient rtuClient) rtuClient.Connect(SerialPortName);
+    }
+
+    private decimal RegisterToWeight(int[] weightRegisters)
+    {
+        var weight = (ushort) weightRegisters[0];
+        var isNegative = Convert.ToBoolean(weight >> 15);
+        if (isNegative) weight = (ushort) (weight & 0x7FFF);
+
+        decimal pointValue = Convert.ToDecimal((ushort) weightRegisters[1]) / ushort.MaxValue;
+        decimal result = Convert.ToDecimal(weight) + pointValue;
+        return result;
+    }
+
+    /// <summary>
+    ///     Get the current reading of LM8MyPin asynchronously
+    /// </summary>
+    /// <param name="unitIdentifier">The unit identifier or address of the LM8MyPin hardware</param>
+    /// <returns>Current weight</returns>
+    public async Task<decimal> GetWeightAsync(byte unitIdentifier)
+    {
+        int[] weightRegisters = (await ModbusClient.ReadHoldingRegistersAsync<int>(unitIdentifier, 98, 2)).ToArray();
+        return RegisterToWeight(weightRegisters);
+    }
+
+    /// <summary>
+    ///     Get the current reading of LM8MyPin synchronously. Consider using asynchronous version whenever possible.
+    /// </summary>
+    /// <param name="unitIdentifier">The unit identifier or address of the LM8MyPin hardware</param>
+    /// <returns>Current weight</returns>
+    public decimal GetWeight(byte unitIdentifier)
+    {
+        int[] weightRegisters = ModbusClient.ReadHoldingRegisters<int>(unitIdentifier, 98, 2).ToArray();
+        return RegisterToWeight(weightRegisters);
+    }
+}
